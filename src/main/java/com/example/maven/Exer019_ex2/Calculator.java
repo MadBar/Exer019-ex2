b5fae56ad3c2c949e694fce84caf0574feb6b110
Madeleine Barwén <barwen@outlook.com>
2017-11-15 10:24:13 +0100

    changed name

---
+++ src/main/java/com/example/maven/Exer019_ex2/Calculator.java
@@ -0,0 +1,5 @@
+package com.example.maven.Exer019_ex2;
+
+public class Calculator {
+
+}
---
+++ src/main/java/com/example/maven/Exer019_ex2/Main.java
@@ -0,0 +1,10 @@
+package com.example.maven.Exer019_ex2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
